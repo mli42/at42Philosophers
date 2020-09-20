b1bb0268b24bc254f3908b51890bc79dd387a6ae
mli42 <mli@student.42.fr>
2020-09-20 16:30:02 +0200

    timestamp is precise

--- timestamp.c
+++ timestamp.c
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/16 13:25:10 by mli               #+#    #+#             */
-/*   Updated: 2020/09/16 13:28:59 by mli              ###   ########.fr       */
+/*   Updated: 2020/09/20 16:29:24 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,36 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-double	get_time(void)
+unsigned long int	get_time(void)
 {
 	struct timeval	tp;
-	double			milliseconds;
 
 	gettimeofday(&tp, NULL);
-	milliseconds = tp.tv_sec * 1000;
-	milliseconds += (double)tp.tv_usec / 1000;
-	return (milliseconds);
+	return ((tp.tv_sec * 1000) + (tp.tv_usec / 1000));
 }
 
-void	dosleep(int const microseconds)
+void				dosleep(const unsigned int microseconds)
 {
-	static const int	divide = 0;
-	const int			cut = microseconds >> divide;
-	usleep(cut);
-//	usleep(cut);
+	const unsigned long int	milliseconds = microseconds / 1000;
+	const unsigned long int start = get_time();
+
+	while (get_time() - start < milliseconds)
+		usleep(400);
 }
 
-int		main(void)
+int					main(void)
 {
-	double	start_time;
-	double	curr_time;
-	double	prev_time;
+	unsigned int	start_time;
+	unsigned int	curr_time;
+	unsigned int	prev_time;
 
 	prev_time = 0;
 	start_time = get_time();
 	while (1)
 	{
-	//	static int i = 0;
 		curr_time = get_time() - start_time;
 		// Print time from start, in ms
-		printf("%ld\t\t[%lf]\n", (long int)curr_time, curr_time - prev_time - 200);
+		printf("%ld\t\t[%d]\n", (long int)curr_time, curr_time - prev_time);
 		prev_time = curr_time;
 		// Sleep 200 times 1000 microseconds (1 millisecond)
 		dosleep(200 * 1000);
